68c7bf156af433991dd57b88b58b4cbb83d677dd
shubh <shubhambhakuni26@gmail.com>
2020-09-03 16:10:01 +0530

    updated index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/header.php'); ?>
+
+	<h4 class="center grey-text">Auction!</h4>
+
+	<div class="container">
+		<div class="row">
+
+		
+
+		</div>
+	</div>
+
+	<?php include('templates/footer.php'); ?>
+
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,44 @@
+<?php 
+
+	if(isset($_POST['submit'])){
+
+		//cookie for gender
+		setcookie('gender', $_POST['gender'], time() + 86400);
+
+
+		header('Location: index.php');
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php tuts</title>
+</head>
+<body>
+
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+		<input type="text" name="name">
+		<select name="gender">
+			<option value="male">male</option>
+			<option value="female">female</option>
+		</select>
+		<input type="submit" name="submit" value="submit">
+	</form>
+
+</body>
+</html>
+
+<!-- <?php 
+
+  // get cookie
+  $gender = $_COOKIE['gender'] ?? 'NULL';
+  $name = $_COOKIE['name'] ?? 'Unkown';
+
+?>
+
+
 <head>
 	<title>Auction Ninja</title>
 	<!-- Compiled and minified CSS -->
@@ -22,7 +63,11 @@
     <div class="container">
       <a href="#" class="brand-logo brand-text">Auction Ninja</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
+		<li class="grey-text"><?php echo htmlspecialchars($name); ?></li>
+
+		<li class="grey-text"> (<?php echo htmlspecialchars($gender); ?> )</li>
+
         <li><a href="#" class="btn brand z-depth-0">Check for New Auctions</a></li>
       </ul>
     </div>
-  </nav>
+  </nav> -->
